1ead340224db7341eacadb0075e5f8651926c00c
Derek Leung <dleung00@stuy.edu>
2019-09-18 14:52:18 -0400

    moved code from computer

---
+++ 08_app0/file.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello_world():
+  #print(__name__)
+  print("bruh")
+  return "No hablo queso!"
+
+@app.route("/2")
+
+def hello_world2():
+    print("bruh2")
+    return "it was so good they had to make a second one"
+
+@app.route("/3")
+
+def hello_world3():
+    print("bruh3")
+    return "now it's a trilogy"
+    
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
