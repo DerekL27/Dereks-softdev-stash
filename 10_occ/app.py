1cbc217cba07747bab58a814824bc608de85f4c5
Derek Leung <dleung00@stuy.edu>
2019-09-20 14:56:59 -0400

    added more code

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -6,16 +6,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
+
 @app.route("/")
 def hello_world():
     return "No hablo queso!"
 
 coll = [0,1,1,2,3,5,8]
+coll2= []
 
 @app.route("/my_foist_template")
 def test_tmplt():
     return render_template(
     'thing.html',
+    foo = "fooooo",
+    collection = coll
+    )
+
+@app.route("/occupyflaskst")
+def test_tmplt2():
+    return render_template(
+    'thing2.html',
     foo = "fooooo",
     collection = coll
     )
