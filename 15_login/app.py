e99ee11dda5a1805e9bd0eb25c1af4c6b6fa523e
Derek Leung <dleung00@stuy.edu>
2019-10-02 18:56:28 -0400

    did code skeleton?

---
+++ 15_login/app.py
@@ -0,0 +1,12 @@
+#Derek Leung and Lauren Pehlivanian
+#SoftDev1 pd 9
+#K#15 -- Do I Know You?
+#2019-10-04
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def something():
+    return hi
