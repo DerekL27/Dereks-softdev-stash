a8a69c107abc4e533edba138a707ae2a9d0f8fbe
Derek Leung <dleung00@stuy.edu>
2019-10-03 14:50:58 -0400

    got login page to redirect

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,10 +3,40 @@
 #K#15 -- Do I Know You?
 #2019-10-04
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def something():
-    return hi
+    return redirect("/login")
+
+@app.route("/login")
+def something2():
+    # print(url_for("something2"))
+    # print(url_for("something"))
+    # print(url_for("something3"))
+    # print(url_for("something4"))
+    return render_template("login.html")
+
+@app.route("/error")
+def something3():
+    return render_template("error.html")
+
+@app.route("/welcome")
+def something4():
+    return render_template("welcome.html")
+
+username = "hi"
+
+@app.route("/auth")
+def something5():
+    print(request.headers)
+    print(request.args)
+    if (request.args['user']==username):
+        return render_template("welcome.html")
+    return render_template("error.html")
+
+if __name__ == "__main__":
+    app.debug = True;
+    app.run()
