1ff53a850805fa03e28249071d68156aefc33085
Derek Leung <dleung00@stuy.edu>
2019-10-03 21:00:12 -0400

    added cookie function

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,13 +3,22 @@
 #K#15 -- Do I Know You?
 #2019-10-04
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
+
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(32)
+username = "hi"
+password = "hi"
+
 @app.route("/")
 def something():
-    return redirect("/login")
+    if("username" in session and "password" in session):
+        if(session['username'] == username and session['password'] == password):
+            return redirect("/welcome")
+    else: return redirect("/login")
 
 @app.route("/login")
 def something2():
@@ -17,7 +26,7 @@ def something2():
     # print(url_for("something"))
     # print(url_for("something3"))
     # print(url_for("something4"))
-    return render_template("login.html")
+    return render_template("login.html", uMatch=True, pMatch=True)
 
 @app.route("/error")
 def something3():
@@ -25,17 +34,23 @@ def something3():
 
 @app.route("/welcome")
 def something4():
-    return render_template("welcome.html")
+    if("username" in session):
+        ussername = session['username']
+    else: ussername = None
+    return render_template("welcome.html",username=ussername)
 
-username = "hi"
 
 @app.route("/auth")
 def something5():
-    print(request.headers)
-    print(request.args)
-    if (request.args['user']==username):
-        return render_template("welcome.html")
-    return render_template("error.html")
+    #print(request.headers)
+    #print(request.args)
+    userPassed = request.args['user']==username
+    passPassed = request.args['pass']==password
+    if (userPassed and passPassed):
+        session['username'] = username
+        session['password'] = password
+        return render_template("welcome.html",username=username)
+    return render_template("login.html", uMatch=userPassed, pMatch=passPassed)
 
 if __name__ == "__main__":
     app.debug = True;
