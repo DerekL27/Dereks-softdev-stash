a1e605bcbf17cd9076b2697983d9083d9406d97c
Derek Leung <dleung00@stuy.edu>
2020-02-11 09:19:48 -0500

    finished code

--- 06_canvas/dotconn.js
+++ 06_canvas/dotconn.js
@@ -1,38 +1,42 @@
-//Derek Leung and William Lin
-//Softdev1 pd2
-//K04: I See a Red Door...
-//2020-02-06
+/*
+William Lin, Derek Leung
+SoftDev Pd. 2
+K #06: Dot Dot Dot
+2020-02-11
+*/
 
-//e.preventDefault() throws out the event if it is not explicitly handled in the code
+var xcoor = null;
+var ycoor = null;
 
-var rectMode = false //state variable?
 
 var clearScreen = function(e){
-  var c = document.getElementById("slate");
+  var c = document.getElementById("playground");
   var ctx = c.getContext("2d");
   ctx.clearRect(0,0,c.width,c.height);
+	xcoor = null;
+	ycoor = null;
 };
 
 var button = document.getElementById('clear');
 button.addEventListener('click',clearScreen);
 
-var switchMode = function(e){
-    if(rectMode == false){
-        rectMode = true;
-    }
-    else{
-        rectMode = false;
-    }
-};
-
 
 var draw = function(event){
-    var c = document.getElementById("slate");
+    var c = document.getElementById("playground");
     var ctx = c.getContext("2d");
+    var tempx = event.offsetX
+    var tempy = event.offsetY
     ctx.beginPath(); //like pd in netlogo?
     ctx.arc(event.offsetX, event.offsetY, 5, 0, 2 * Math.PI);
     ctx.fill();
+    ctx.moveTo(tempx,tempy);
+		if (xcoor != null && ycoor != null){
+			ctx.lineTo(xcoor, ycoor);
+			ctx.stroke();
+		}
+		xcoor = event.offsetX;
+		ycoor = event.offsetY;
 };
 
-var canvas = document.getElementById("slate");
-canvas.addEventListener('click',draw);
+var canvas = document.getElementById("playground");
+canvas.addEventListener('click', draw);
