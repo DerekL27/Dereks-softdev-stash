f821f13ef132cb1ebbee81d0a76c0c0c25b5f3de
Derek Leung <dleung00@stuy.edu>
2020-02-13 08:52:21 -0500

    added comments

--- 07_canvas-anim/animate.js
+++ 07_canvas-anim/animate.js
@@ -12,40 +12,48 @@ var animationId;
 var radius = 0;
 var adjust = 1;
 
-var animate1 = function(e){
-    if(!isAnimating){
-    window.requestAnimationFrame(animate);
-    isAnimating = true;
-}
+var animateStart = function(e) {
+    // Checks if already animating so clicking the animate button again won't break things
+    if (!isAnimating) {
+        window.requestAnimationFrame(animate);
+        isAnimating = true;
+    }
 };
 
-var animate = function(e){
-    if(radius>=300){
-     adjust = -1;
+var animate = function(e) {
+    // If the current radius of the drawn circle hits any edge, start shrinking the circle
+    if (radius >= Math.round((Math.min(canvas.width, canvas.height)) / 2)) {
+        adjust = -1;
     }
-    if(radius<=2){
-     adjust = 1;
+    // If the current radius of the drawn circle is 0, start expanding the circle
+    if (radius <= 0) {
+        adjust = 1;
     }
+    // Adjust the intended radius of the next circle to be drawn
     radius+=adjust;
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    ctx.beginPath();    // starts to draw the circle/dot (like netlogo 'pd' or 'pen down')
-    ctx.arc(300, 300, radius, 0, 6.28);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);   // Clears the canvas first
+    // Draws the new circle centered on the canvas with the new radius
+    ctx.beginPath();
+    ctx.arc((canvas.width / 2), (canvas.height / 2), radius, 0, 6.28);
     ctx.fillStyle = "#D8E4FF";
     ctx.fill();
     animationId = window.requestAnimationFrame(animate);
 };
 
 var animateButton = document.getElementById("animate");
-animateButton.addEventListener("click", animate1);
+animateButton.addEventListener("click", animateStart);
 
 
 
- var stop = function(e){
-     if (isAnimating){
-         isAnimating = false;
-         window.cancelAnimationFrame(animationId);
-     }
- };
+var stop = function(e) {
 
- var stopButton = document.getElementById("stop");
- stopButton.addEventListener("click", stop);
+    // Checks if already animating so clicking the animate button again won't break things
+    if (isAnimating) {
+        isAnimating = false;
+        window.cancelAnimationFrame(animationId);
+    }
+};
+
+var stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", stop);
