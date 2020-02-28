f78516804829a5a384a7a6276557de883e5d8c40
KingDerek1 <dleung00@stuy.edu>
2020-02-28 03:23:00 +0000

    hi

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -2,5 +2,15 @@ from bson import json_util
 from pymongo import MongoClient
 client = MongoClient()
 db = client.test
-collection = db.info
+collection = db.restaurants
+if(collection.count() == 0):
+	file = open('primer-dataset.json','r')
+	bruh = file.readlines()
+	for line in bruh:
+		collection.insert_one(loads(line))
 
+def getBorough(borough):
+	bruh = collection.find({"borough": borough})
+	print(bruh)
+
+getBorough("Bronx")
