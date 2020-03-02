a5c07dfdb9b464fe327f70df9406592439994cfa
Derek Leung <dleung00@stuy.edu>
2020-03-01 19:17:39 -0500

    added stuff

---
+++ 10_mongo/mongo.py
@@ -0,0 +1,13 @@
+#Derek Leung, Alex Thompson
+#Softdeb pd 2
+#K10 -- Import/Export Bank
+#2020-03-04
+
+import json
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.teamName
+collection = db.events
+if(collection.count() == 0):
+    pass
