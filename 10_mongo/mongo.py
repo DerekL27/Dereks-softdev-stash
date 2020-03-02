7b223bf317873be56a7c2b8dddfd11d6462f09d8
Derek Leung <dleung00@stuy.edu>
2020-03-02 10:57:02 -0500

    did a bit of code

--- 10_mongo/mongo.py
+++ 10_mongo/mongo.py
@@ -3,6 +3,12 @@
 #K10 -- Import/Export Bank
 #2020-03-04
 
+"""
+prize.json contains all nobel prize winners, as well as the year in which they won the prize and what they won the prize for.
+http://api.nobelprize.org/v1/prize.json
+To import the data, we first opened the json and read in the lines to a list. Then we used the json library to convert the json strings to dictionaries and inserted them one by one into the database.
+"""
+
 import json
 from pymongo import MongoClient
 
@@ -10,9 +16,16 @@ client = MongoClient()
 db = client.teamName
 collection = db.events
 if(collection.count() == 0):
-    file = open("englishEvents.json", "r")
+    file = open("prize.json", "r")
     content = file.readlines()
     for line in content:
         collection.insert_one(loads(line))
 
-def findBetweenYears()
+def findTopic(topic):
+    topic = topic.lower()
+    data = collection.find({"category": topic})
+    for thing in data:
+        print(data)
+        print('\n')
+
+findTopic("chemistry")
